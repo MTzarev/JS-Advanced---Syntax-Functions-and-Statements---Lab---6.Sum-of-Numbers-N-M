e8a15dffd036e9bc9ac9977c3083447714f1a924
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-05 23:25:30 +0300

    Add files via upload

---
+++ 6.Sum of Numbers N…M.js
@@ -0,0 +1,13 @@
+function sumOfNumbersNM(str1, str2) {
+str1=Number(str1)
+str2=Number(str2)
+let result=0
+for (let i=str1; i<=str2; i++){
+    let num=Number(i)
+    result+=num
+
+}
+
+    console.log(result);
+}
+sumOfNumbersNM('-8', `20`);
